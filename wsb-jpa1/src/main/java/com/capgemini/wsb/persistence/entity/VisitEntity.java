704807ff58273adefe89d0a3d387b11626dcae8a
Paweł Gawlik <pgawlik215@gmail.com>
2024-03-26 22:02:37 +0100

    first commit

---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/entity/VisitEntity.java
@@ -0,0 +1,52 @@
+package com.capgemini.wsb.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "VISIT")
+public class VisitEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+
+	@Column(nullable = false)
+	private LocalDateTime time;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "DOCTOR_ID")
+	private DoctorEntity doctorEntity;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "Patient_ID")
+	private PatientEntity patientEntity;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+
+}
